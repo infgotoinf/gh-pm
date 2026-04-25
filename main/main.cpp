8c490f820b37d85afbf6b2423fa22fcb99bbd4ee
-inf <infgotoinf@gmail.com>
2026-04-25 22:59:17 +0300

    chore: change debendencees, update placeholder programm, to make it GET request to Github GraphQL API and print result on the TUI screen

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,11 @@
 // Copyright 2020 Arthur Sonzogni. All rights reserved.
 // Use of this source code is governed by the MIT license that can be found in
 // the LICENSE file.
-#include <ftxui/screen/screen.hpp>
 // #include <memory>  // for allocator, __shared_ptr_access
+#include <cstdio>
 #include <string>  // for char_traits, operator+, string, basic_string
 
+#include <ftxui/screen/screen.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/component/event.hpp>
 #include <ftxui/component/captured_mouse.hpp>  // for ftxui
@@ -14,55 +15,34 @@
 #include <ftxui/dom/elements.hpp>  // for text, hbox, separator, Element, operator|, vbox, border
 #include <ftxui/util/ref.hpp>  // for Ref
 
+#include <nlohmann/json.hpp>
+
 int main() {
   using namespace ftxui;
 
-  // The data:
-  std::string first_name;
-  std::string last_name;
-  std::string password;
-  std::string phoneNumber;
+  FILE *p;
+  std::string querry;
 
-  // The basic input components:
-  Component input_first_name = Input(&first_name, "first name");
-  Component input_last_name = Input(&last_name, "last name");
+  p = popen("gh api graphql -f query='query { viewer { projectsV2(first:100) { nodes { id title } } } }'","r");
 
-  // The password input component:
-  InputOption password_option;
-  password_option.password = true;
-  Component input_password = Input(&password, "password", password_option);
+  if( p == NULL)
+  {
+      puts("Unable to open process");
+      return 1;
+  }
 
-  // The phone number input component:
-  // We are using `CatchEvent` to filter out non-digit characters.
-  Component input_phone_number = Input(&phoneNumber, "phone number");
-  input_phone_number |= CatchEvent([&](Event event) {
-    return event.is_character() && !std::isdigit(event.character()[0]);
-  });
-  input_phone_number |= CatchEvent([&](Event event) {
-    return event.is_character() && phoneNumber.size() > 10;
-  });
+  char line[256];
+  while(fgets(line, 256, p) != nullptr)
+      querry += line;
+  pclose(p);
 
-  // The component tree:
-  auto component = Container::Vertical({
-      input_first_name,
-      input_last_name,
-      input_password,
-      input_phone_number,
-  });
-
-  // Tweak how the component tree is rendered:
-  auto renderer = Renderer(component, [&] {
-    return vbox({
-               hbox(text(" First name : "), input_first_name->Render()),
-               hbox(text(" Last name  : "), input_last_name->Render()),
-               hbox(text(" Password   : "), input_password->Render()),
-               hbox(text(" Phone num  : "), input_phone_number->Render()),
-               separator(),
-               text("Hello " + first_name + " " + last_name),
-               text("Your password is " + password),
-               text("Your phone number is " + phoneNumber),
-           }) |
-           border;
+  auto renderer = Renderer([&]
+  {
+      return vbox(
+      {
+          paragraph(querry)
+      })
+      | border;
   });
 
   auto screen = ScreenInteractive::Fullscreen();
